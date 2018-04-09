5ecd79c5ff61745dc3316ab63772aeb589dc0c33
lcd344 <lcd344@yahoo.com>
2018-04-09 13:11:36 -0500

    fix

--- resources/views/store/product/table.blade.php
+++ resources/views/store/product/table.blade.php
@@ -20,7 +20,7 @@
                             <p class="control">
                                 <input class="input{{ $errors->has("{$variant->title}_{$part}_min") ? ' is-danger' : ''}}"
                                        name="{{ $variant->title }}_{{ $part }}_min"
-                                       value="{{ old("{$variant->title}_{$part}_min", $productModel->data[$variant->title . '_' . $part . '_min']) }}">
+                                       value="{{ old("{$variant->title}_{$part}_min", $productModel->data[$variant->title . '_' . $part . '_min'] ?? '') }}">
                             </p>
                             @if ($errors->has("{$variant->title}_{$part}_min"))
                                 <p class="help is-danger">{{ $errors->first("{$variant->title}_{$part}_min") }}</p>
@@ -31,7 +31,7 @@
                             <p class="control">
                                 <input class="input{{ $errors->has("{$variant->title}_{$part}_max") ? ' is-danger' : ''}}"
                                        name="{{ $variant->title }}_{{ $part }}_max"
-                                       value="{{ old("{$variant->title}_{$part}_max", $productModel->data[$variant->title . '_' . $part . '_max']) }}">
+                                       value="{{ old("{$variant->title}_{$part}_max", $productModel->data[$variant->title . '_' . $part . '_max'] ?? '') }}">
                             </p>
                             @if ($errors->has("{$variant->title}_{$part}_max"))
                                 <p class="help is-danger">{{ $errors->first("{$variant->title}_{$part}_max") }}</p>
