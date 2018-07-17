61ae207d0c5c3f66b928f3ad219efd4c626cecf2
Nur Bar <LCD344@yahoo.com>
2018-07-17 21:52:32 +0100

    added tawk embed code

--- resources/views/store/product.blade.php
+++ resources/views/store/product.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.store')
-
+@section('head')
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+		var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+		(function () {
+			var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+			s1.async = true;
+			s1.src = 'https://embed.tawk.to/5a65fa52d7591465c706faa6/default';
+			s1.charset = 'UTF-8';
+			s1.setAttribute('crossorigin', '*');
+			s0.parentNode.insertBefore(s1, s0);
+		})();
+    </script>
+    <!--End of Tawk.to Script-->
+@endsection
 @section('content')
     <store-product-page inline-template
                         start-metric="{{ old('measurement', $productModel->measurement ?? 'metric') == 'metric' ? 'metric' : 'imperial'}}">
