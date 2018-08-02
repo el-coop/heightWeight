0bb85750b8ee65bace9ac5739d7ece0128207057
Nur Bar <LCD344@yahoo.com>
2018-08-02 21:19:45 +0200

    started working

--- resources/assets/js/components/client/measurements/Shirt.vue
+++ resources/assets/js/components/client/measurements/Shirt.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="columns is-mobile">
-        <div class="column" style="position: relative">
-            <figure class="image is-square">
-                <img src="/images/shirt-measurments.jpg">
-            </figure>
-            <input class="input sleeve is-small" v-model="sleeve"/>
-            <input class="input bust is-small" v-model="bust"/>
-            <input class="input length is-small" v-model="length"/>
-            <input class="input waist is-small" v-model="waist"/>
-        </div>
+	<div class="columns is-mobile">
+		<div class="column" style="position: relative">
+			<figure class="image is-square">
+				<img src="/images/shirt-measurments.jpg">
+			</figure>
+			<input class="input sleeve is-small" v-model="sleeve"/>
+			<input class="input bust is-small" v-model="bust"/>
+			<input class="input length is-small" v-model="length"/>
+			<input class="input waist is-small" v-model="waist"/>
+		</div>
 
-        <div class="column is-3 is-flex" style="align-items: center">
-            <div class="field">
-                <div class="control">
-                    <div class="select">
-                        <select v-model="measuredCategory">
-                            <option v-for="size in sizes" :value="size" v-html="size"></option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+		<div class="column is-3 is-flex" style="align-items: center">
+			<div class="field">
+				<div class="control">
+					<div class="select">
+						<select v-model="measuredCategory">
+							<option v-for="size in sizes" :value="size" v-html="size"></option>
+						</select>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -89,6 +89,7 @@
 				let resultCategory = this.findCategorySize('height', this.userData.height);
 				let weightCategory = this.findCategorySize('weight', this.userData.weight);
 				if (resultCategory !== weightCategory) {
+					resultCategory = Math.max(resultCategory, weightCategory);
 					if (resultCategory !== 0) {
 						if (this.userData.bmi < 18.5) {
 							resultCategory--;
@@ -143,7 +144,8 @@
 						waistCategory = this.findCategorySize('waist', this.waist);
 					}
 					return this.sizes[Math.max(sleeveCategory, bustCategory, lengthCategory, waistCategory)];
-				},
+				}
+				,
 				set(val) {
 					this.sleeve = parseFloat(this.product.data[val].sleeve.min || 0);
 					this.bust = parseFloat(this.product.data[val].bust.min || 0);
@@ -152,41 +154,42 @@
 					return val;
 				}
 			}
-		},
+		}
+		,
 
 	}
 </script>
 
 <style lang="scss" scoped>
-    div {
-        position: relative;
-    }
+	div {
+		position: relative;
+	}
 
-    .sleeve {
-        position: absolute;
-        width: 4ch;
-        top: 42%;
-    }
+	.sleeve {
+		position: absolute;
+		width: 4ch;
+		top: 42%;
+	}
 
-    .bust {
-        position: absolute;
-        width: 5ch;
-        top: 44%;
-        left: 40%;
-    }
+	.bust {
+		position: absolute;
+		width: 5ch;
+		top: 44%;
+		left: 40%;
+	}
 
-    .length {
-        position: absolute;
-        width: 5ch;
-        top: 65%;
-        left: 55%;
-    }
+	.length {
+		position: absolute;
+		width: 5ch;
+		top: 65%;
+		left: 55%;
+	}
 
-    .waist {
-        position: absolute;
-        width: 5ch;
-        top: 73%;
-        left: 38%;
-    }
+	.waist {
+		position: absolute;
+		width: 5ch;
+		top: 73%;
+		left: 38%;
+	}
 
 </style>
