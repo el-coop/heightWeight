98271d8fbb4efb3960de5155858e470f36a863a8
Nur Bar <LCD344@yahoo.com>
2018-08-23 16:00:52 +0700

    added Handy theme query selector to tag

--- resources/assets/js/scriptTag.js
+++ resources/assets/js/scriptTag.js
@@ -1,7 +1,7 @@
 let url = 'https://heightweight.test';
 //let url = 'https://app.seezerapps.com';
 let embedTag = document.querySelector('#height-weight');
-let checkoutForm = document.querySelector('.product-form, #AddToCartForm, .product__form');
+let checkoutForm = document.querySelector('.product-form, #AddToCartForm, .product__form, .product-form-inline');
 
 
 if (checkoutForm || embedTags) {
